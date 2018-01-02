34f47d738e5c6da0563f3d9420da97b6dc92c916
Sreenadh TC <sreenadh@datahut.co>
2018-01-02 14:49:00 +0530

    Add Day 31 - Update cart api for delete api, update put api with better db performance

--- 024/ecommerce/modules/order.py
+++ 024/ecommerce/modules/order.py
@@ -74,6 +74,16 @@ class Cart(Resource):
 			pid = li.get('pid', '')
 			try:
 				product = ProductModel.objects.get(pid = pid)
+				item_total = product.price * qty
+				
+				item = {
+					'product':product.pid,
+					'name':product.name,
+					'qty':qty,
+					'rate':product.price,
+					'total': item_total
+				}
+
 			except DoesNotExist:
 				cart.update(items = [])
 				cart.update(total = 0)
@@ -82,32 +92,71 @@ class Cart(Resource):
 					404
 				)
 
-			item_total = product.price * qty
-			current_total += item_total
+			try:
+				cart = CartModel.objects.get(customer = customer, items__product__in = [pid])
+				for itm in cart.items:
+					if itm.get('product') == pid:
+						itm_price = itm.get('total')
+						cart.update(dec__total = itm_price)
+						cart.update(inc__total = item_total)
 
-			cart.reload()
-			if cart.items:
-				for curr in cart.items:
-					if curr['product'] == product.pid:
-						curr['qty'] = qty
-						curr['total'] = item_total
-						current_items.append(curr)
-			else:
-				item = {
-					'product':product.pid,
-					'name':product.name,
-					'qty':qty,
-					'rate':product.price,
-					'total': item_total
-				}
-				current_items.append(item)
+						cart.update(pull__items = itm)
 
-		cart.update(items = current_items)
-		cart.update(total = current_total)
+						if item_total > 0:
+							cart.update(add_to_set__items = item)
+
+			except DoesNotExist:
+				cart.update(inc__total = item_total)
+				cart.update(add_to_set__items = item)
 
 		cart.reload()
 		cart = cart.to_mongo().to_dict()
 		cart.pop('_id')
 		cart.pop('customer')
 		return jsonify(cart = cart)
-		
+
+	def delete(self):
+		data = request.get_json()
+
+		try:
+			access_key = data.get('access_key')
+			clear = data.get('empty', False)
+			pid = data.get('product_id')
+
+			assert access_key
+			assert pid
+
+			if not authorize_access_key(access_key):
+				return make_response(
+					jsonify(message="Unauthorised"),
+					403
+				)
+		except AssertionError:
+			return make_response(
+				jsonify(message = "Invalid request"),
+				400
+			)
+
+		try:
+			customer = UserModel.objects.get(access_key = access_key)
+			cart = CartModel.objects.get(customer = customer, items__product__in = [pid])
+		except DoesNotExist:
+			return make_response(
+				jsonify(message = "Not found"),
+				404
+			)
+		_cart_items = cart.items
+		_total = 0
+
+		cart_items = []
+		for item in _cart_items:
+			if item.get('product') != pid:
+				_total += item.get('total')
+				cart_items.append(item)
+		cart.update(items = cart_items)
+		cart.update(total = _total)
+		cart.reload()
+
+		total = cart.total
+		cart = cart.items
+		return jsonify(cart = cart, total = total)
